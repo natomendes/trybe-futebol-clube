44bd6a442f872f99b4a2b38671d673b4fc3f07a2
Renato Mendes <mendeslrenato@gmail.com>
2022-11-22 23:19:10 -0300

    test: ensure SignInController (integration) returns 400 if no email is provided

--- app/backend/src/tests/signin-controller.test.ts
+++ app/backend/src/tests/signin-controller.test.ts
@@ -1,49 +1,66 @@
-// import * as sinon from 'sinon';
-// import * as chai from 'chai';
-// // @ts-ignore
-// import chaiHttp = require('chai-http');
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
 
-// import App from '../app';
-// import User from '../database/models/User';
+import App from '../app';
+import User from '../database/models/User';
 
-// import { Response } from 'superagent';
+import { Response } from 'superagent';
+import SignInController from '../presentation/controllers/signin-controller';
+import { response } from 'express';
 
-// chai.use(chaiHttp);
+chai.use(chaiHttp);
 
-// const { app } = new App();
+const { app } = new App();
 
-// const { expect } = chai;
+const { expect } = chai;
 
-// describe('Seu teste', () => {
-//   /**
-//    * Exemplo do uso de stubs com tipos
-//    */
+const makeSut = (): SignInController => {
+  return new SignInController();
+}
 
-//   let chaiHttpResponse: Response;
+describe('Seu teste', () => {
+  /**
+   * Exemplo do uso de stubs com tipos
+   */
 
-//   before(async () => {
-//     sinon
-//       .stub(UserMySqlRepository, "find")
-//       .resolves({
-//         id: 1,
-//         username: 'any_name',
-//         role: 'any_roll',
-//         email: 'valid_email@mail.com',
-//       } as UserModel);
-//   });
+  let chaiHttpResponse: Response;
 
-//   after(()=>{
-//     (UserMySqlRepository.find as sinon.SinonStub).restore();
-//   })
+  // before(async () => {
+  //   sinon
+  //     .stub(UserMySqlRepository, "find")
+  //     .resolves({
+  //       id: 1,
+  //       username: 'any_name',
+  //       role: 'any_roll',
+  //       email: 'valid_email@mail.com',
+  //     } as UserModel);
+  // });
 
-//   it('Should return 400 if no email is provided', async () => {
-//     chaiHttpResponse = await chai
-//        .request(app)
-//        .post('/login')
-//        .send({
-//         password: 'any_password',
-//       })
+  // after(()=>{
+  //   (UserMySqlRepository.find as sinon.SinonStub).restore();
+  // })
 
-//     expect(chaiHttpResponse.status).to.be.equal(400);
-//   });
-// });
+  it('Should return 400 if no email is provided', async () => {
+    app.post('/login', async (req, res) => {
+      const sut = makeSut();
+      const httpRequest = {
+        body: {
+          password: 'any_password',
+        }
+      };
+      const response = await sut.handle(httpRequest);
+      console.log(response)
+      res.status(response.statusCode).send();
+    })
+    chaiHttpResponse = await chai
+       .request(app)
+       .post('/login')
+       .send({
+        password: 'any_password',
+      })
+
+    expect(chaiHttpResponse.status).to.be.equal(400);
+  });
+});
