ea2b50920ce5b0e4b460942a498af9d284095228
Renato Mendes <mendeslrenato@gmail.com>
2022-11-23 18:32:13 -0300

    test: ensure UserRepository call model.findOne with correct email

--- app/backend/tests/infra/db/user-repository/user.spec.ts
+++ app/backend/tests/infra/db/user-repository/user.spec.ts
@@ -9,8 +9,15 @@ describe('UserRepository', () => {
   it('Should return undefined if no user is found', async () => {
     const sut = makeSut();
     jest.spyOn(User, 'findOne')
-      .mockResolvedValueOnce(null)
-    const user = await sut.find('valid_mail@mail.com')
+      .mockResolvedValueOnce(null);
+    const user = await sut.find('valid_mail@mail.com');
     expect(user).toBeUndefined();
-  })
-})
+  });
+
+  it('Should call model with correct email', async () => {
+    const sut = makeSut();
+    const findOneSpy = jest.spyOn(User, 'findOne')
+    await sut.find('valid_mail@mail.com');
+    expect(findOneSpy).toHaveBeenCalledWith({ where: { email: 'valid_mail@mail.com' } });
+  });
+});
