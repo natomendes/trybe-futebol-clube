63e6798880ac84364275783d69a521b176dcc8f4
Renato Mendes <mendeslrenato@gmail.com>
2022-11-24 22:47:37 -0300

    test: ensure AuthenticationController returns unauthorized if token is invalid

--- app/backend/tests/presentation/controllers/auth-controller.spec.ts
+++ app/backend/tests/presentation/controllers/auth-controller.spec.ts
@@ -1,14 +1,30 @@
-import { MissingTokenError } from '../../../src/presentation/errors';
+import { MissingTokenError, InvalidTokenError } from '../../../src/presentation/errors';
 import AuthenticationController from '../../../src/presentation/controllers/auth-controller'
+import { TokenValidator } from '../../../src/presentation/protocols/token';
+import { JwtPayload, TokenExpiredError } from 'jsonwebtoken';
+
+const makeTokenValidatorStub = (): TokenValidator => {
+  class TokenValidatorStub implements TokenValidator {
+    validate(token: string): JwtPayload {
+      return {
+        email: 'valid_mail@mail.com'
+      }
+    }
+  }
+  return new TokenValidatorStub();
+}
 
 interface SutTypes {
+  tokenValidatorStub: TokenValidator
   sut: AuthenticationController
 }
 
 const makeSut = (): SutTypes => {
-  const sut = new AuthenticationController();
+  const tokenValidatorStub = makeTokenValidatorStub();
+  const sut = new AuthenticationController(tokenValidatorStub);
   return {
     sut,
+    tokenValidatorStub,
   }
 }
 
@@ -21,5 +37,19 @@ describe('AuthenticantionController', () => {
     const httpResponse = await sut.handle(httpResquest);
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual({ message: new MissingTokenError().message})
+  });
+
+  it('Should return unauthorized if token is invalid', async () => {
+    const { sut, tokenValidatorStub } = makeSut();
+    jest.spyOn(tokenValidatorStub, 'validate')
+      .mockImplementationOnce(() => { throw new TokenExpiredError('jwt expired', new Date())})
+    const httpResquest = {
+      headers: { 
+        authorization: 'invalid_token'
+      }
+    }
+    const httpResponse = await sut.handle(httpResquest);
+    expect(httpResponse.statusCode).toBe(401);
+    expect(httpResponse.body).toEqual({ message: new InvalidTokenError().message})
   });
 })
