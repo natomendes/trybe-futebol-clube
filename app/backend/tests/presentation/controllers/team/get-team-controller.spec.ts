9401cc849231e2ec9d93168516f6bbd08edd2a9d
Renato Mendes <mendeslrenato@gmail.com>
2022-11-25 19:39:47 -0300

    test: ensure GetTeamController returns not found if no team is found with provided id

--- app/backend/tests/presentation/controllers/team/get-team-controller.spec.ts
+++ app/backend/tests/presentation/controllers/team/get-team-controller.spec.ts
@@ -1,13 +1,27 @@
+import { TeamModel } from '../../../../src/domain/models/team';
+import { FindTeam } from '../../../../src/domain/usecases/find-teams';
 import GetTeamController from '../../../../src/presentation/controllers/team/get-team-controller'
+import { teamMock } from '../../../mocks/team-model-mock'
+const makeFindTeamStub = (): FindTeam => {
+  class FindTeamStub implements FindTeam {
+    async find(id: string): Promise<TeamModel | undefined> {
+      return teamMock;
+    }
+  }
+  return new FindTeamStub();
+}
 
 interface SutTypes {
   sut: GetTeamController
+  findTeamStub: FindTeam
 }
 
 const makeSut = (): SutTypes => {
-  const sut = new GetTeamController();
+  const findTeamStub = makeFindTeamStub();
+  const sut = new GetTeamController(findTeamStub);
   return {
-    sut
+    sut,
+    findTeamStub
   }
 }
 
@@ -22,5 +36,18 @@ describe('GetTeamController', () => {
     const httpResponse = await sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual({ message: 'Missing param "id"'});
-  })
+  });
+
+  it('Should return not found if no team is found with the id provided', async () => {
+    const { sut, findTeamStub } = makeSut();
+    jest.spyOn(findTeamStub, 'find').mockResolvedValueOnce(undefined);
+    const httpRequest = {
+      params: {
+        id: 'no_team_id',
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(404);
+    expect(httpResponse.body).toEqual({ message: 'Team not found'});
+  });
 });
