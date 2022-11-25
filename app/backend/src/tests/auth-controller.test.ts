7cc1c2e3886dc8e38dee3b3d8f24b83450b846b6
Renato Mendes <mendeslrenato@gmail.com>
2022-11-25 15:58:00 -0300

    test: create AuthenticationController integration tests

---
+++ app/backend/src/tests/auth-controller.test.ts
@@ -0,0 +1,185 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import App from '../app';
+import User from '../database/models/User';
+
+import { Response } from 'superagent';
+import { InvalidParamError, InvalidTokenError, MissingTokenError, ServerError } from '../presentation/errors';
+import DbFindUser from '../data/usecases/find-user/db-find-user';
+import JwtAdapter from '../utils/jwt-adapter';
+import { TokenExpiredError } from 'jsonwebtoken';
+
+
+chai.use(chaiHttp);
+
+const { app } = new App();
+
+const { expect } = chai;
+
+describe('AuthenticationController', () => {
+  let chaiHttpResponse: Response;
+
+  beforeEach(async () => {
+    sinon
+      .stub(User, "findOne")
+      .resolves({
+        id: 1,
+        username: 'any_name',
+        role: 'any_roll',
+        email: 'valid_email@mail.com',
+      } as User);
+  });
+
+  afterEach(()=>{
+    (User.findOne as sinon.SinonStub).restore();
+    sinon.restore();
+  });
+  
+  it('Should return bad request if no token is provided', async () => {
+
+    chaiHttpResponse = await chai
+       .request(app)
+       .get('/login/validate')
+       .send();
+
+    expect(chaiHttpResponse.status).to.be.equal(400);
+    expect(chaiHttpResponse.body)
+      .to.be.deep.equal({ message: new MissingTokenError().message});
+  });
+
+  it('Should return not found if no user is found in the database', async () => {
+    sinon.stub(JwtAdapter.prototype, 'validate').returns({ email: 'valid_mail@mail.com'});
+    sinon.stub(DbFindUser.prototype, 'find').resolves(undefined);
+    chaiHttpResponse = await chai
+       .request(app)
+       .get('/login/validate')
+       .set('authorization', 'valid_token')
+       .send();
+
+    expect(chaiHttpResponse.status).to.be.equal(404);
+    expect(chaiHttpResponse.body)
+      .to.be.deep.equal({ message: new InvalidParamError('User not found').message});
+  });
+
+  it('Should return unauthorized if token is invalid', async () => {
+    sinon.stub(JwtAdapter.prototype, 'validate').throws(new TokenExpiredError('InvalidToken', new Date()));
+    chaiHttpResponse = await chai
+       .request(app)
+       .get('/login/validate')
+       .set('authorization', 'invalid_token')
+       .send();
+
+    expect(chaiHttpResponse.status).to.be.equal(401);
+    expect(chaiHttpResponse.body)
+      .to.be.deep.equal({ message: new InvalidTokenError().message});
+  });
+
+  it('Should return server error if FindUser throws', async () => {
+    sinon.stub(DbFindUser.prototype, 'find').throws();
+    chaiHttpResponse = await chai
+       .request(app)
+       .get('/login/validate')
+       .set('authorization', 'invalid_token')
+       .send();
+
+    expect(chaiHttpResponse.status).to.be.equal(500);
+    expect(chaiHttpResponse.body)
+      .to.be.deep.equal({ message: new ServerError().message});
+  });
+
+  it('Should return user role on success', async () => {
+    sinon.stub(JwtAdapter.prototype, 'validate').returns({ email: 'valid_mail@mail.com'});
+    chaiHttpResponse = await chai
+       .request(app)
+       .get('/login/validate')
+       .set('authorization', 'valid_token')
+       .send();
+
+    expect(chaiHttpResponse.status).to.be.equal(200);
+    expect(chaiHttpResponse.body)
+      .to.be.deep.equal({ role: 'any_roll' });
+  });
+
+  // it('Should return 400 and and error if an invalid email is provided', async () => {
+  //   sinon.stub(EmailValidatorAdapter.prototype, 'isValid').returns(false);
+  //   chaiHttpResponse = await chai
+  //      .request(app)
+  //      .post('/login')
+  //      .send({
+  //       email: 'invalid_email@mail.com',
+  //       password: 'any_password',
+  //     });
+
+  //   expect(chaiHttpResponse.status).to.be.equal(400);
+  //   expect(chaiHttpResponse.body)
+  //     .to.be.deep.equal({ message: new InvalidParamError().message});
+  // });
+
+  // it('Should return 401 and an error if email provided has no match in the database', async () => {
+  //   sinon.stub(DbFindUser.prototype, 'find')
+  //     .resolves(undefined);
+  //   chaiHttpResponse = await chai
+  //      .request(app)
+  //      .post('/login')
+  //      .send({
+  //       email: 'no_user@mail.com',
+  //       password: 'any_password',
+  //     });
+
+  //   expect(chaiHttpResponse.status).to.be.equal(401);
+  //   expect(chaiHttpResponse.body)
+  //     .to.be.deep.equal({ message: new InvalidParamError().message});
+  // });
+
+  // it('Should return 401 and an error if password provided is wrong', async () => {
+  //   sinon.stub(BcryptAdapter.prototype, 'validate')
+  //     .resolves(false);
+  //   chaiHttpResponse = await chai
+  //      .request(app)
+  //      .post('/login')
+  //      .send({
+  //       email: 'valid_email@mail.com',
+  //       password: 'wrong_password',
+  //     });
+
+  //   expect(chaiHttpResponse.status).to.be.equal(401);
+  //   expect(chaiHttpResponse.body)
+  //     .to.be.deep.equal({ message: new InvalidParamError().message});
+  // });
+
+  // it('Should return an token on success', async () => {
+  //   sinon.stub(BcryptAdapter.prototype, 'validate')
+  //   .resolves(true);
+  //   sinon.stub(JwtAdapter.prototype, 'generate')
+  //   .returns('valid_token');
+  //   chaiHttpResponse = await chai
+  //      .request(app)
+  //      .post('/login')
+  //      .send({
+  //       email: 'valid_email@mail.com',
+  //       password: 'valid_password',
+  //     });
+
+  //   expect(chaiHttpResponse.status).to.be.equal(200);
+  //   expect(chaiHttpResponse.body)
+  //     .to.be.deep.equal({ token: 'valid_token' });
+  // });
+
+  // it('Should return 500 and an error if something throws and exception', async () => {
+  //   sinon.stub(BcryptAdapter.prototype, 'validate').throws()
+  //   chaiHttpResponse = await chai
+  //      .request(app)
+  //      .post('/login')
+  //      .send({
+  //       email: 'valid_email@mail.com',
+  //       password: 'valid_password',
+  //     });
+
+  //   expect(chaiHttpResponse.status).to.be.equal(500);
+  //   expect(chaiHttpResponse.body)
+  //     .to.be.deep.equal({ message: new ServerError().message });
+  // });
+});
