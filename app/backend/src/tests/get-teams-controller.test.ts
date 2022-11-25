af576a82dfff8577a0ff08b7f22f69c17d39ba7e
Renato Mendes <mendeslrenato@gmail.com>
2022-11-25 19:01:21 -0300

    test: create GetTeamsController integration tests

---
+++ app/backend/src/tests/get-teams-controller.test.ts
@@ -0,0 +1,62 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import App from '../app';
+import Team from '../database/models/Team';
+
+import { Response } from 'superagent';
+import { getTeamsMock } from '../../tests/mocks/team-model-mock'
+import { InvalidParamError, InvalidTokenError, MissingTokenError, ServerError } from '../presentation/errors';
+import DbFindTeams from '../data/usecases/find-teams/db-find-teams'
+chai.use(chaiHttp);
+
+const { app } = new App();
+
+const { expect } = chai;
+
+describe('GetTeamsController', () => {
+  let chaiHttpResponse: Response;
+
+  afterEach(()=>{
+    sinon.restore();
+  });
+  
+  it('Should return server error if FindTeams throws', async () => {
+    sinon.stub(DbFindTeams.prototype, 'find').throws();
+    chaiHttpResponse = await chai
+       .request(app)
+       .get('/teams')
+       .send();
+
+    expect(chaiHttpResponse.status).to.be.equal(500);
+    expect(chaiHttpResponse.body)
+      .to.be.deep.equal({ message: 'Internal server error'});
+  });
+  
+  it('Should return an empty array if no teams are found', async () => {
+    sinon.stub(Team, "findAll").resolves([]);
+    chaiHttpResponse = await chai
+       .request(app)
+       .get('/teams')
+       .send();
+
+    expect(chaiHttpResponse.status).to.be.equal(200);
+    expect(chaiHttpResponse.body)
+      .to.be.deep.equal([]);
+  });
+  
+  it('Should return all teams on success', async () => {
+    sinon.stub(Team, "findAll").resolves(getTeamsMock as Team[]);
+    chaiHttpResponse = await chai
+       .request(app)
+       .get('/teams')
+       .send();
+
+    expect(chaiHttpResponse.status).to.be.equal(200);
+    expect(chaiHttpResponse.body)
+      .to.be.deep.equal(getTeamsMock);
+  });
+});
+
