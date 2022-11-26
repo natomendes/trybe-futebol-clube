a188d1fd6c5d29f2bb0f559d1ac4b00bfa53ecee
Renato Mendes <mendeslrenato@gmail.com>
2022-11-25 22:07:04 -0300

    feat: create GetTeamController factory

---
+++ app/backend/src/main/factories/get-team-controller.ts
@@ -0,0 +1,11 @@
+import TeamRepository from '../../infra/db/team-repository/team';
+import DbFindTeam from '../../data/usecases/find-teams/db-find-team';
+import GetTeamController from '../../presentation/controllers/team/get-team-controller';
+
+const makeGetTeamController = (): GetTeamController => {
+  const findTeamRepository = new TeamRepository();
+  const findTeam = new DbFindTeam(findTeamRepository);
+  return new GetTeamController(findTeam);
+};
+
+export default makeGetTeamController;
