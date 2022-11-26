9a0dc2c534bb449e6f3735e5a4a243d5ae744a4c
Renato Mendes <mendeslrenato@gmail.com>
2022-11-26 16:56:42 -0300

    test: ensure AddMatchController returns badRequest if no token is provided

--- app/backend/tests/presentation/controllers/match/add-match-controller.spec.ts
+++ app/backend/tests/presentation/controllers/match/add-match-controller.spec.ts
@@ -1,7 +1,10 @@
 import { TokenValidator } from '../../../../src/presentation/controllers/match/match-protocols';
 import { JwtPayload, TokenExpiredError } from 'jsonwebtoken';
 import AddMatchController from '../../../../src/presentation/controllers/match/add-match-controller';
-
+import {
+  invalidTokenHttpRequest,
+  missingTokenHttpRequest,
+} from '../../../mocks/match-model-mock';
 const makeTokenValidatorStub = (): TokenValidator => {
   class TokenValidatorStub implements TokenValidator {
     validate(token: string): JwtPayload {
@@ -29,22 +32,18 @@ const makeSut = (): SutTypes => {
 
 describe('AddMatchController', () => {
   it('Should return unauthorized if token is invalid', async () => {
+    const { sut } = makeSut();
+    const httpResponse = await sut.handle(missingTokenHttpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual({ message: 'Token is required' });
+  });
+
+  it('Should return unauthorized if token is invalid', async () => {
     const { sut, tokenValidatorStub } = makeSut();
     jest.spyOn(tokenValidatorStub, 'validate')
       .mockImplementationOnce(() => { throw new TokenExpiredError('jwt expired', new Date())});
-    const httpResquest = {
-      headers: { 
-        authorization: 'invalid_token'
-      },
-      body: {
-        homeTeam: 16,
-        awayTeam: 8,
-        homeTeamGoals: 2,
-        awayTeamGoals: 2,
-      }
-    };
-    const httpResponse = await sut.handle(httpResquest);
+    const httpResponse = await sut.handle(invalidTokenHttpRequest);
     expect(httpResponse.statusCode).toBe(401);
     expect(httpResponse.body).toEqual({ message: 'Token must be a valid token' });
-  })
+  });
 });
