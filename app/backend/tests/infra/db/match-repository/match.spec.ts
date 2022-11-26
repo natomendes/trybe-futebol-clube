80e6dbb1bccab736de11642ad689c05603f04817
Renato Mendes <mendeslrenato@gmail.com>
2022-11-26 12:59:42 -0300

    test: ensure FindMatchesRepository return matches filtered if query inProgress is provided

--- app/backend/tests/infra/db/match-repository/match.spec.ts
+++ app/backend/tests/infra/db/match-repository/match.spec.ts
@@ -1,6 +1,6 @@
 import Match from '../../../../src/database/models/Match';
 import MatchRepository from '../../../../src/infra/db/match-repository/match';
-import { allMatchesMock } from '../../../mocks/match-model-mock';
+import { allMatchesMock, matchesInProgressMock } from '../../../mocks/match-model-mock';
 
 describe('MatchRepository', () => {
   it('Should throw if Match throws', async () => {
@@ -17,6 +17,14 @@ describe('MatchRepository', () => {
         .mockResolvedValueOnce([]);
       const matches = await sut.findAll();
       expect(matches).toEqual([]);
+    });
+
+    it('Should return matches filtered if query inProgress is provided', async () => {
+      const sut = new MatchRepository();
+      jest.spyOn(Match, 'findAll')
+        .mockResolvedValueOnce(matchesInProgressMock as any);
+      const matches = await sut.findAll();
+      expect(matches).toEqual(matchesInProgressMock);
     });
 
     it('Should return all matches on success', async () => {
