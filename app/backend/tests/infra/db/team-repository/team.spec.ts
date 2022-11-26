92366dde2296976ef484e39fa285c6d71aad5e28
Renato Mendes <mendeslrenato@gmail.com>
2022-11-25 21:59:21 -0300

    test: ensure TeamRepository findOne returns undefined if no team is found

--- app/backend/tests/infra/db/team-repository/team.spec.ts
+++ app/backend/tests/infra/db/team-repository/team.spec.ts
@@ -1,6 +1,6 @@
 import Team from '../../../../src/database/models/Team';
 import TeamRepository from '../../../../src/infra/db/team-repository/team';
-import { getTeamsMock } from '../../../mocks/team-model-mock'
+import { getTeamsMock, teamMock } from '../../../mocks/team-model-mock'
 
 describe('TeamRepository', () => {
   it('Should throw if Team throws', async () => {
@@ -10,20 +10,41 @@ describe('TeamRepository', () => {
     const promise = sut.findAll();
     await expect(promise).rejects.toThrow();
   });
-
-  it('Should return an empty array if no teams are found', async () => {
-    const sut = new TeamRepository();
-    jest.spyOn(Team, 'findAll')
-      .mockResolvedValueOnce([]);
-    const teams = await sut.findAll();
-    expect(teams).toEqual([]);
+  
+  describe('findAll method', () => {
+    it('Should return an empty array if no teams are found', async () => {
+      const sut = new TeamRepository();
+      jest.spyOn(Team, 'findAll')
+        .mockResolvedValueOnce([]);
+      const teams = await sut.findAll();
+      expect(teams).toEqual([]);
+    });
+  
+    it('Should return all teams on success', async () => {
+      const sut = new TeamRepository();
+      jest.spyOn(Team, 'findAll')
+        .mockResolvedValueOnce(getTeamsMock as Team[]);
+      const teams = await sut.findAll();
+      expect(teams).toEqual(getTeamsMock);
+    });
   });
 
-  it('Should return all teams on success', async () => {
-    const sut = new TeamRepository();
-    jest.spyOn(Team, 'findAll')
-      .mockResolvedValueOnce(getTeamsMock as Team[]);
-    const teams = await sut.findAll();
-    expect(teams).toEqual(getTeamsMock);
+  describe('findOne method', () => {
+    it('Should return undefined if no team is found', async () => {
+      const sut = new TeamRepository();
+      jest.spyOn(Team, 'findOne')
+        .mockResolvedValueOnce(null);
+      const team = await sut.findOne('no_team_id');
+      expect(team).toEqual(undefined);
+    });
+
+    // it('Should return a team on success', async () => {
+    //   const sut = new TeamRepository();
+    //   jest.spyOn(Team, 'findOne')
+    //     .mockResolvedValueOnce(teamMock as Team);
+    //   const team = await sut.findOne('valid_id');
+    //   expect(team).toEqual(teamMock);
+    // });
   });
+  
 });
