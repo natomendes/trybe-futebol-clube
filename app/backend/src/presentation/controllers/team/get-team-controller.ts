565eec03e0cff11a5422fa7547bb07196deac05d
Renato Mendes <mendeslrenato@gmail.com>
2022-11-25 21:22:31 -0300

    feat: ensure GetTeamController returns server error if FindTeam throws

--- app/backend/src/presentation/controllers/team/get-team-controller.ts
+++ app/backend/src/presentation/controllers/team/get-team-controller.ts
@@ -1,17 +1,21 @@
-import MissingParamError, { InvalidParamError } from '../../errors';
-import { badRequest, notFound, ok } from '../../helpers/http-helpers';
+import MissingParamError, { InvalidParamError, ServerError } from '../../errors';
+import { badRequest, notFound, ok, serverError } from '../../helpers/http-helpers';
 import { HttpRequest, HttpResponse } from '../sign-in/signin-protocols';
 import { Controller, FindTeam } from './get-teams-protocols';
 
 export default class GetTeamController implements Controller {
   constructor(private findTeam: FindTeam) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { id } = httpRequest.params;
-    if (!id) return badRequest(new MissingParamError('id'));
+    try {
+      const { id } = httpRequest.params;
+      if (!id) return badRequest(new MissingParamError('id'));
 
-    const team = await this.findTeam.find(id);
-    if (!team) return notFound(new InvalidParamError('Team not found'));
+      const team = await this.findTeam.find(id);
+      if (!team) return notFound(new InvalidParamError('Team not found'));
 
-    return ok(team);
+      return ok(team);
+    } catch (error) {
+      return serverError(new ServerError());
+    }
   }
 }
