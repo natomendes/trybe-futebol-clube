1668946bbc979f75986350e213e91a7bc1bd8140
Renato Mendes <mendeslrenato@gmail.com>
2022-11-26 01:36:22 -0300

    test: ensure DbFindMatches returns all matches on success

--- app/backend/tests/data/usecases/find-matches/db-find-matches.spec.ts
+++ app/backend/tests/data/usecases/find-matches/db-find-matches.spec.ts
@@ -1,7 +1,8 @@
-import { FindMatchesRepository } from '../../../../src/data/protocols/find-matches-repository'
-import { MatchModel } from '../../../../src/domain/models/match'
-import { allMatchesMock } from '../../../mocks/match-model-mock'
-import DbFindMatches from '../../../../src/data/usecases/find-matches/db-find-matches'
+import { FindMatchesRepository } from '../../../../src/data/protocols/find-matches-repository';
+import { MatchModel } from '../../../../src/domain/models/match';
+import { allMatchesMock } from '../../../mocks/match-model-mock';
+import DbFindMatches from '../../../../src/data/usecases/find-matches/db-find-matches';
+
 const makeFindMatchesRepositoryStub = (): FindMatchesRepository => {
   class FindMatchesRepositoryStub implements FindMatchesRepository {
     async findAll(): Promise<MatchModel[]> {
@@ -33,5 +34,11 @@ describe('DbFindMatches', () => {
       .mockRejectedValueOnce(new Error());
     const promise = sut.find();
     await expect(promise).rejects.toThrow();
-    });
+  });
+
+  it('Should return all matches on success', async () => {
+    const { sut } = makeSut();
+    const matches = await sut.find();
+    expect(matches).toEqual(allMatchesMock);
+  });
 });
