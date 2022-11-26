620026d24c7363c54f2ebe448c08e269cee48dda
Renato Mendes <mendeslrenato@gmail.com>
2022-11-25 21:33:39 -0300

    feat: ensure DbFindTeam throws if FindTeamRepository throws

--- app/backend/src/data/protocols/find-teams-repository.ts
+++ app/backend/src/data/protocols/find-teams-repository.ts
@@ -3,3 +3,7 @@ import { TeamModel } from '../../domain/models/team';
 export interface FindTeamsRepository {
   findAll(): Promise<TeamModel[]>
 }
+
+export interface FindTeamRepository {
+  find(id: string): Promise<TeamModel | undefined>
+}
---
+++ app/backend/src/data/usecases/find-teams/db-find-team.ts
@@ -0,0 +1,10 @@
+import { FindTeamRepository } from '../../protocols/find-teams-repository';
+import { TeamModel } from '../../../domain/models/team';
+import { FindTeam } from '../../../domain/usecases/find-teams';
+
+export default class DbFindTeam implements FindTeam {
+  constructor(private findTeamRepository: FindTeamRepository) {}
+  async find(id: string): Promise<TeamModel | undefined> {
+    return this.findTeamRepository.find(id);
+  }
+}
