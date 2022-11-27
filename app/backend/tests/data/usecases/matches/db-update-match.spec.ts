171e14db693dac742f508f066b22fc264a81edff
Renato Mendes <mendeslrenato@gmail.com>
2022-11-27 20:10:54 -0300

    test: ensure DbUpdateMatch calls UpdateMatchRepository with correct values

---
+++ app/backend/tests/data/usecases/matches/db-update-match.spec.ts
@@ -0,0 +1,43 @@
+import { UpdateMatchRepository } from '../../../../src/data/protocols'
+import { UpdateMatchModel } from '../../../../src/domain/usecases';
+import DbUpdateMatch from '../../../../src/data/usecases/matches/db-update-match';
+
+const makeUpdateMatchRepository = (): UpdateMatchRepository => {
+  class UpdateMatchRepositoryStub implements UpdateMatchRepository {
+    async update(updateMatchdata: UpdateMatchModel): Promise<number> {
+      return 1;
+    }
+  }
+  return new UpdateMatchRepositoryStub();
+}
+
+interface SutTypes {
+  sut: DbUpdateMatch,
+  updateMatchRepositoryStub: UpdateMatchRepository
+}
+
+const makeSut = (): SutTypes => {
+  const updateMatchRepositoryStub = makeUpdateMatchRepository();
+  const sut = new DbUpdateMatch(updateMatchRepositoryStub);
+  return {
+    sut,
+    updateMatchRepositoryStub
+  }
+}
+
+describe('DbUpdateMatch', () => {
+  it('Should call MatchRepository with correct values', async () => {
+    const { sut, updateMatchRepositoryStub } = makeSut();
+    const updateSpy = jest.spyOn(updateMatchRepositoryStub, 'update');
+    await sut.update({
+      id: '1',
+      homeTeamGoals: '1',
+      awayTeamGoals: '1',
+    });
+    expect(updateSpy).toHaveBeenCalledWith({
+      id: '1',
+      homeTeamGoals: '1',
+      awayTeamGoals: '1',
+    });
+  });
+});
