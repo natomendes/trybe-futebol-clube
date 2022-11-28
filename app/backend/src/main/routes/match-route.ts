2af7cb1249f767e3020ada17eab4de6cecf576fe
Renato Mendes <mendeslrenato@gmail.com>
2022-11-27 22:44:44 -0300

    feat: create update matches route

--- app/backend/src/main/routes/match-route.ts
+++ app/backend/src/main/routes/match-route.ts
@@ -2,8 +2,13 @@ import { Router } from 'express';
 import adaptRoute from '../adapters/express-route-adapter';
 import makeAddMatchController from '../factories/add-match-controller';
 import makeGetMatchesController from '../factories/get-matches-controller';
+import makeUpdateMatchController from '../factories/update-match-controller';
 
 export default (router: Router): void => {
   router.get('/matches', adaptRoute(makeGetMatchesController()));
   router.post('/matches', adaptRoute(makeAddMatchController()));
+  router.patch('/matches/:id', adaptRoute(makeUpdateMatchController()));
+  router.patch('/matches/:id/finish', async (_req, res) => {
+    res.status(200).json({});
+  });
 };
