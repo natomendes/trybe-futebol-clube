55884f15510143c2b87e5b6bc92203447ca8ba9d
Renato Mendes <mendeslrenato@gmail.com>
2022-11-30 01:21:55 -0300

    test: ensure DbGetLeaderboard calls GetLeaderboardStats with correct values

--- app/backend/tests/data/usecases/teams/db-get-leaderboard.spec.ts
+++ app/backend/tests/data/usecases/teams/db-get-leaderboard.spec.ts
@@ -1,6 +1,6 @@
 import { StatsModel } from "../../../../src/domain/models";
-import { GetTeamsStats } from "../../../../src/domain/usecases";
-import { awayStatsMock, homeStatsMock } from "../../../mocks/leaderboard-model-mock";
+import { GetTeamsStats, GetLeaderboardStats } from "../../../../src/domain/usecases";
+import { awayStatsMock, homeStatsMock, leaderboardMock } from "../../../mocks/leaderboard-model-mock";
 import DbGetLeaderboard from '../../../../src/data/usecases/teams/db-get-leaderboard';
 
 const makeGetTeamsStatsSutb = (): GetTeamsStats => {
@@ -17,32 +17,51 @@ const makeGetTeamsStatsSutb = (): GetTeamsStats => {
   return new GetTeamsStatsStub();
 }
 
+const makeGetLeaderboardStatsStub = (): GetLeaderboardStats => {
+  class GetLeaderboardStatsStub implements GetLeaderboardStats {
+    async handle(homeStats: StatsModel[], awayStats: StatsModel[]): Promise<StatsModel[]> {
+      return leaderboardMock;
+    }
+  }
+  return new GetLeaderboardStatsStub();
+}
+
 interface SutTypes {
   sut: DbGetLeaderboard
   getTeamsStatsStub: GetTeamsStats
+  getLeaderboardStatsStub: GetLeaderboardStats
 }
 
 const makeSut = (): SutTypes => {
+  const getLeaderboardStatsStub = makeGetLeaderboardStatsStub();
   const getTeamsStatsStub = makeGetTeamsStatsSutb();
-  const sut = new DbGetLeaderboard(getTeamsStatsStub);
+  const sut = new DbGetLeaderboard(getTeamsStatsStub, getLeaderboardStatsStub);
   return {
     sut,
-    getTeamsStatsStub
+    getTeamsStatsStub,
+    getLeaderboardStatsStub
   }  
 }
 
 describe('DbGetLeaderboard', () => {
-  it('Should call DbGetTeamsStats with "home" argument on the first call', async () => {
+  it('Should call GetTeamsStats with "home" argument on the first call', async () => {
     const { sut, getTeamsStatsStub } = makeSut();
     const handleSpy = jest.spyOn(getTeamsStatsStub, 'handle');
     await sut.handle();
     expect(handleSpy).toHaveBeenNthCalledWith(1, 'home');
   });
 
-  it('Should call DbGetTeamsStats with "away" argument on the second call', async () => {
+  it('Should call GetTeamsStats with "away" argument on the second call', async () => {
     const { sut, getTeamsStatsStub } = makeSut();
     const handleSpy = jest.spyOn(getTeamsStatsStub, 'handle');
     await sut.handle();
     expect(handleSpy).toHaveBeenNthCalledWith(2, 'away');
+  });
+
+  it('Should call GetLeaderboardStats with correct values', async () => {
+    const { sut, getLeaderboardStatsStub } = makeSut();
+    const handleSpy = jest.spyOn(getLeaderboardStatsStub, 'handle');
+    await sut.handle()
+    expect(handleSpy).toHaveBeenCalledWith(homeStatsMock, awayStatsMock);
   });
 });
